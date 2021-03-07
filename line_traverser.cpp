45a6bf7efbed0edba8a0d9c5180961af5a6eb6b6
Jordan Leggett <jleggett@ucsc.edu>
2021-03-06 20:46:41 -0800

    comments + get endpoints function

--- line_traverser.cpp
+++ line_traverser.cpp
@@ -1,29 +1,40 @@
 
+/// line_traverser.cpp
+/// Provides functions for traversing a line over a grid, where the starting and ending points
+/// have sub-grid coordinates.
+
 #include "line_traverser.h"
 #include <math.h>
 
-LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width)
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+#ifndef max
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width)
 {
     LineTraverser traverser;
     int dx = x2 - x1;
     int dy = y2 - y1;
     traverser.dx_x = (dx >= 0) ? 1 : -1;
     traverser.dy_y = (dy >= 0) ? 1 : -1;
-    int local_x = x1 % pixel_width;
-    int local_y = y1 % pixel_width;
-    int x_dist = (dx >= 0) ? (pixel_width - local_x) : (local_x);
-    int y_dist = (dy >= 0) ? (pixel_width - local_y) : (local_y);
+    int local_x = x1 % square_width;
+    int local_y = y1 % square_width;
+    int x_dist = (dx >= 0) ? (square_width - local_x) : (local_x);
+    int y_dist = (dy >= 0) ? (square_width - local_y) : (local_y);
     traverser.clockwiseness = (int64_t)abs(dx) * abs(y_dist) - (int64_t)abs(dy) * abs(x_dist);
-    traverser.dx_clockwiseness = -(int64_t)abs(dy) * pixel_width;
-    traverser.dy_clockwiseness = (int64_t)abs(dx) * pixel_width;
+    traverser.dx_clockwiseness = -(int64_t)abs(dy) * square_width;
+    traverser.dy_clockwiseness = (int64_t)abs(dx) * square_width;
 
-    traverser.x = x1 / pixel_width;
-    traverser.y = y1 / pixel_width;
-    traverser.end_x = x2 / pixel_width;
-    traverser.end_y = y2 / pixel_width;
+    traverser.x = x1 / square_width;
+    traverser.y = y1 / square_width;
+    traverser.end_x = x2 / square_width;
+    traverser.end_y = y2 / square_width;
     if (dy < 0)
     {
-        if ((y1 % pixel_width) == 0)
+        if ((y1 % square_width) == 0)
         {
             traverser.y--;
             traverser.clockwiseness += traverser.dy_clockwiseness;
@@ -31,13 +42,13 @@ LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
     }
     else if (dy > 0)
     {
-        if ((y2 % pixel_width) == 0)
+        if ((y2 % square_width) == 0)
             traverser.end_y--;
     }
 
     if (dx < 0)
     {
-        if ((x1 % pixel_width) == 0)
+        if ((x1 % square_width) == 0)
         {
             traverser.x--;
             traverser.clockwiseness += traverser.dx_clockwiseness;
@@ -45,7 +56,7 @@ LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
     }
     else if (dx > 0)
     {
-        if ((x2 % pixel_width) == 0)
+        if ((x2 % square_width) == 0)
             traverser.end_x--;
     }
     return traverser;
@@ -72,18 +83,73 @@ void LineTraverser_next(LineTraverser *p_traverser)
     }
 }
 
-
 void LineTraverser_get_point(const LineTraverser *p_traverser, int32_t *out_x, int32_t *out_y)
 {
     *out_x = p_traverser->x;
     *out_y = p_traverser->y;
 }
 
+void LineTraverser_get_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, bool include_endpoints,
+    int32_t *out_x1, int32_t *out_y1, int32_t *out_x2, int32_t *out_y2)
+{
+    int32_t dx = x2 - x1;
+    int32_t dy = y2 - y1;
+    int32_t endpoint1_x = x1 / square_width;
+    int32_t endpoint1_y = y1 / square_width;
+    int32_t endpoint2_x = x2 / square_width;
+    int32_t endpoint2_y = y2 / square_width;
+    if (dx > 0)
+    {
+        if ((x2 % square_width) == 0)
+            endpoint2_x--;
+        if (!include_endpoints)
+        {
+            endpoint2_x--;
+            endpoint1_x++;
+        }
+    }
+    else if (dx < 0)
+    {
+        if ((x1 % square_width) == 0)
+            endpoint1_x--;
+        if (!include_endpoints)
+        {
+            endpoint1_x--;
+            endpoint2_x++;
+        }
+    }
 
-void LineTraverser_traverse_include_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width, 
+    if (dy > 0)
+    {
+        if ((y2 % square_width) == 0)
+            endpoint2_y--;
+        if (!include_endpoints)
+        {
+            endpoint2_y--;
+            endpoint1_y++;
+        }
+    }
+    else if (dy < 0)
+    {
+        if ((y1 % square_width) == 0)
+            endpoint1_y--;
+        if (!include_endpoints)
+        {
+            endpoint1_y--;
+            endpoint2_y++;
+        }
+    }
+    *out_x1 = min(endpoint1_x, endpoint2_x);
+    *out_x1 = max(endpoint1_x, endpoint2_x);
+    *out_y2 = min(endpoint1_y, endpoint2_y);
+    *out_y2 = max(endpoint1_y, endpoint2_y);
+}
+
+
+void LineTraverser_traverse_include_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, 
     LineTraverserCallback callback, void *user_data)
 {
-    LineTraverser traverser = LineTraverser_init(x1, y1, x2, y2, pixel_width);
+    LineTraverser traverser = LineTraverser_init(x1, y1, x2, y2, square_width);
     while (true)
     {
         int32_t x, y;
@@ -95,10 +161,10 @@ void LineTraverser_traverse_include_endpoints(int32_t x1, int32_t y1, int32_t x2
     }
 }
 
-void LineTraverser_traverse_exclude_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width, 
+void LineTraverser_traverse_exclude_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, 
     LineTraverserCallback callback, void *user_data)
 {
-    LineTraverser traverser = LineTraverser_init(x1, y1, x2, y2, pixel_width);
+    LineTraverser traverser = LineTraverser_init(x1, y1, x2, y2, square_width);
     if (LineTraverser_is_end(&traverser))
         return;
     while (true)
--- line_traverser.h
+++ line_traverser.h
@@ -1,4 +1,8 @@
 
+/// line_traverser.h
+/// Provides functions for traversing a line over a grid, where the starting and ending points
+/// have sub-grid coordinates.
+
 #ifndef LINE_TRAVERSER_H
 #define LINE_TRAVERSER_H
 
@@ -15,17 +19,104 @@ typedef struct
     int32_t end_x, end_y;
 } LineTraverser;
 
-LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width);
+/// Initializes a LineTraverser for a given line.
+/// @param x1 is the x coordinate of the starting point of the line.
+/// @param y1 is the y coordinate of the starting point of the line.
+/// @param x2 is the x coordinate of the ending point of the line.
+/// @param y2 is the y coordinate of the ending point of the line.
+/// @param square_width is the width of a square in the grid being traversed.
+/// @returns a LineTraverser which can be used to traverse all grid points which intersect the line.
+/// @remarks This will traverse every grid point which intersects the line. If the line intersects only
+/// a corner of a grid square, that grid square will not be traversed. If the line intersects the bottom (min-y)
+/// side of a grid square and is perfectly horizontal, that grid square will be traversed.
+/// If the line intersects the left (min-x) side of a grid square and is perfectly vertical,
+/// that grid square will be traversed.
+LineTraverser LineTraverser_init(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width);
+
+/// Tests if the current grid square in the traverser is the endpoint of the line.
+/// @param p_traverser is a pointer to the traverser to test.
+/// @returns true if this current grid square is the last one. False otherwise.
+/// @remarks The last grid square corresponds to the point (x2,y2) in the call to LineTraverser_init().
+/// This will only return true ON the last grid square. If LineTraverser_next() is called
+/// past the end of the line, this function will never return true again (until integer overflow).
+/// So you must stop the traversing loop once this returns true, or define your own loop exit condition.
 bool LineTraverser_is_end(const LineTraverser *p_traverser);
+
+/// Gets the current grid square coordinate of a LineTraverser.
+/// @param p_traverser is a pointer to the traverser to get the coordinate of.
+/// @param out_x is a pointer to write the x coordinate to.
+/// @param out_y is a pointer to write the y coordinate to.
 void LineTraverser_get_point(const LineTraverser *p_traverser, int32_t *out_x, int32_t *out_y);
+
+/// Updates a LineTraverser to the next grid coordinate.
+/// @param p_traverser is a pointer to the traverser to update.
 void LineTraverser_next(LineTraverser *p_traverser);
 
+/// Gets the grid-coordinates of the endpoints of a line.
+/// @param x1 is the x coordinate of the starting point of the line.
+/// @param y1 is the y coordinate of the starting point of the line.
+/// @param x2 is the x coordinate of the ending point of the line.
+/// @param y2 is the y coordinate of the ending point of the line.
+/// @param square_width is the width of a square in the grid being traversed.
+/// @param include_endpoints is set to true if you want to consider the endpoints of the line,
+/// or false to exclude the grid squares on the endpoints.
+/// @param out_x1 is a pointer to write the x coordinate which corresponds to x1 in grid-space coordinates.
+/// @param out_y1 is a pointer to write the y coordinate which corresponds to y1 in grid-space coordinates.
+/// @param out_x2 is a pointer to write the x coordinate which corresponds to x2 in grid-space coordinates.
+/// @param out_y2 is a pointer to write the y coordinate which corresponds to y2 in grid-space coordinates.
+/// @remarks It may seem intuitive that the endpoint grid-coordinates can be obtained by dividing the line
+/// endpoints by square_width, however this is not true. This function will get the correct grid coordinates
+/// that corresponds to the line endpoints.
+/// Usually, you will probably want to set include_endpoints to true. But it can be set to false in case you
+/// don't want to include the endpoints. To exclude the endpoints means to skip exactly one square towards
+/// both ends of the line. So, setting include_endpoints to false will exclude exactly one grid square
+/// on each side of the line.
+void LineTraverser_get_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, bool include_endpoints,
+    int32_t *out_x1, int32_t *out_y1, int32_t *out_x2, int32_t *out_y2);
+
+/// User defined function which can be called during a line traversal.
+/// @param x is the x coordinate of current grid square that intersects the line.
+/// @param y is the y coordinate of current grid square that intersects the line.
+/// @param user_data is a pointer to user defined data.
 typedef void (*LineTraverserCallback)(int32_t x, int32_t y, void *user_data);
 
-void LineTraverser_traverse_include_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width, 
+/// Traverser all grid squares that intersects a line.
+/// @param x1 is the x coordinate of the starting point of the line.
+/// @param y1 is the y coordinate of the starting point of the line.
+/// @param x2 is the x coordinate of the ending point of the line.
+/// @param y2 is the y coordinate of the ending point of the line.
+/// @param square_width is the width of a square in the grid being traversed.
+/// @param callback is a user-defined function which will be called for every point on the line.
+/// @param user_data is a pointer to user data passed to callback().
+/// @remarks This will traverse every grid point which intersects the line. If the line intersects only
+/// a corner of a grid square, that grid square will not be traversed. If the line intersects the bottom (min-y)
+/// side of a grid square and is perfectly horizontal, that grid square will be traversed.
+/// If the line intersects the left (min-x) side of a grid square and is perfectly vertical,
+/// that grid square will be traversed.
+/// The meaning of "include_endpoints" is that both starting and ending grid squares will be called during
+/// traversal. The alternative is LineTraverser_traverse_exclude_endpoints() which will exclude exactly one
+/// endpoint on each side of the line.
+void LineTraverser_traverse_include_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, 
     LineTraverserCallback callback, void *user_data);
 
-void LineTraverser_traverse_exclude_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t pixel_width, 
+/// Traverser all grid squares that intersects a line, excluding the starting and ending points.
+/// @param x1 is the x coordinate of the starting point of the line.
+/// @param y1 is the y coordinate of the starting point of the line.
+/// @param x2 is the x coordinate of the ending point of the line.
+/// @param y2 is the y coordinate of the ending point of the line.
+/// @param square_width is the width of a square in the grid being traversed.
+/// @param callback is a user-defined function which will be called for every point on the line.
+/// @param user_data is a pointer to user data passed to callback().
+/// @remarks This will traverse every grid point which intersects the line (excluding endpoints).
+/// If the line intersects only a corner of a grid square, that grid square will not be traversed.
+/// If the line intersects the bottom (min-y) side of a grid square and is perfectly horizontal,
+/// that grid square will be traversed. If the line intersects the left (min-x) side of a grid square
+///  and is perfectly vertical, that grid square will be traversed.
+/// The meaning of "exclude_endpoints" is that both starting and ending grid squares will be excluded during
+/// traversal. They will be excluded, even if the endpoint lies exactly on the edge of a grid square.
+/// The alternative is LineTraverser_traverse_include_endpoints() which will call callback() for every point
+/// that intersects the line. Usually, you probably want to use LineTraverser_traverse_include_endpoints().
+void LineTraverser_traverse_exclude_endpoints(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t square_width, 
     LineTraverserCallback callback, void *user_data);
 
 #endif // LINE_TRAVERSER_H
